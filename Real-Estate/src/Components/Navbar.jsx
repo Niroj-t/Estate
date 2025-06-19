85b15ac2953ec4a2424a9833444d8cd2a3cf3e27
Niroj Thapa <nirojthapa661@gmail.com>
2025-06-19 14:26:28 +0545

    changes commit

--- Real-Estate/src/Components/Navbar.jsx
+++ Real-Estate/src/Components/Navbar.jsx
@@ -46,13 +46,13 @@ const Navbar = () => {
           <a onClick={()=>setShowMobileMenu(false)}  href="#Header" className="px-4 py-2 rounded-full inline-block">
             Home
           </a>
-          <a onClick={()=>setShowMobileMenu(false)} href="#Header" className="px-4 py-2 rounded-full inline-block">
+          <a onClick={()=>setShowMobileMenu(false)} href="#About" className="px-4 py-2 rounded-full inline-block">
             About
           </a>
-          <a onClick={()=>setShowMobileMenu(false)} href="#Header" className="px-4 py-2 rounded-full inline-block">
+          <a onClick={()=>setShowMobileMenu(false)} href="#Projects" className="px-4 py-2 rounded-full inline-block">
             Projects
           </a>
-          <a onClick={()=>setShowMobileMenu(false)}href="#Header" className="px-4 py-2 rounded-full inline-block">
+          <a onClick={()=>setShowMobileMenu(false)}href="#Testimonails" className="px-4 py-2 rounded-full inline-block">
             Testimonails
           </a>
         </ul>
